a5d87e69e0711959277687f7632a39d197bd4479
Shatur95 <genaloner@gmail.com>
2020-04-12 03:02:37 +0300

    Test

--- src/keyboarddaemon.cpp
+++ src/keyboarddaemon.cpp
@@ -43,7 +43,51 @@ void KeyboardDaemon::setGroups(std::vector<std::string> groups)
 {
     m_groups = std::move(groups);
 
+    std::string keycodes = "evdev+aliases(qwerty)";
+    std::string types = "complete";
+    std::string compat = "complete";
+    std::string symbols = "pc+us+ru:2+inet(evdev)+capslock(grouplock)+group(alt_shift_toggle)+terminate(ctrl_alt_bksp)";
+    std::string geometry = "pc(pc101)";
+    XkbComponentNamesRec componentNames = {nullptr, keycodes.data(), types.data(), compat.data(), symbols.data(), geometry.data()};
+    const std::unique_ptr<XkbDescRec, XlibDeleter> clientDesc(XkbGetKeyboardByName(m_display.get(), XkbUseCoreKbd, &componentNames, XkbGBN_AllComponentsMask, XkbGBN_AllComponentsMask, false));
+
+    std:: cout << XkbSetMap(m_display.get(), XkbKeySymsMask, clientDesc.get()) << std::endl;
+    throw std::logic_error("Done!");
+
     // Here need to apply first specified group
+//    const std::unique_ptr<XkbDescRec, XlibDeleter> serverDesc(XkbGetKeyboardByName(m_display.get(), XkbUseCoreKbd, nullptr, XkbGBN_ServerSymbolsMask, XkbGBN_ServerSymbolsMask, false));
+//    const std::unique_ptr<char [], XlibDeleter> symbols(XGetAtomName(m_display.get(), serverDesc->names->symbols));
+
+//    std::cout << symbols.get() << std::endl;
+//    std::string parsedSymbols;
+//    std::replace_copy(symbols.get(), symbols.get() + strlen(symbols.get()), std::back_inserter(parsedSymbols), '_', '+');
+//    std::cout << parsedSymbols << std::endl;
+//    const std::unique_ptr<char [], XlibDeleter> symbols(XGetAtomName(m_display.get(), clientDesc->names->symbols));
+//    std::cout << clientDesc.get() << std::endl;
+
+//    XkbMapChangesRec changes;
+//    changes.changed = XkbVirtualModMapMask;
+//    const std::unique_ptr<char [], XlibDeleter> test(XGetAtomName(m_display.get(), clientDesc->names->symbols));
+//    std::cout << test .get() << std::endl;
+//    XkbChangeMap(m_display.get(), )
+//    std::replace_copy(symbols.get(), strlen(symbols.get()), std::back_inserter(newSymbols), '_', '+');
+//        std::vector<std::string> components;
+//        boost::split(components, group, boost::is_any_of(","));
+//        if (components.size() != 5)
+//            throw std::logic_error(str(boost::format("Specified group %1% have only %2% components (5 expected)") % group % components.size()));
+
+//        XkbComponentNamesRec componentNames;
+//        componentNames.keymap = nullptr;
+//        componentNames.keycodes = components[0].data();
+//        componentNames.types = components[1].data();
+//        componentNames.compat = components[2].data();
+//        componentNames.symbols = components[3].data();
+//        componentNames.geometry = components[4].data();
+
+//        m_groups.emplace_back(XkbGetKeyboardByName(m_display.get(), XkbUseCoreKbd, &componentNames, XkbGBN_AllComponentsMask, XkbGBN_AllComponentsMask, false));
+
+    // Apply first group
+//    XkbSetMap(m_display.get(), XkbGBN_AllComponentsMask & (~XkbGBN_GeometryMask), m_groups.front().get());
 }
 
 void KeyboardDaemon::exec()
